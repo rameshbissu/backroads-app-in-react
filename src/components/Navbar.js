dbdcfb1a99bcc5a7ae74c4bcd2c4df16dca2ff35
rameshbissu <rameshbissu646@gmail.com>
2024-02-16 22:31:20 +0530

    social links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../images/logo.svg";
 import { pageLinks } from "../data";
+import { socialLinks } from "../data";
 
 const Navbar = () => {
   return (
@@ -26,33 +27,21 @@ const Navbar = () => {
         </ul>
 
         <ul className="nav-icons">
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-squarespace"></i>
-            </a>
-          </li>
+          {socialLinks.map((link) => {
+            const { id, href, icon } = link;
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="nav-icon"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
--- src/data.js
+++ src/data.js
@@ -4,3 +4,9 @@ export const pageLinks = [
   { id: 3, href: "#services", text: "service" },
   { id: 4, href: "#tours", text: "tours" },
 ];
+
+export const socialLinks = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
